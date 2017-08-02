694e169dad74230d4714bcd7228df11bdd6641ee
Jung A Park <japark@reconers.com>
2017-08-02 17:20:41 +0900

    안드로이드 사이즈 추가

--- index.js
+++ index.js
@@ -61,18 +61,23 @@ var getPlatforms = function (projectName) {
             { name : 'drawable-land-mdpi/screen.png',  width : 480, height: 320 },
             { name : 'drawable-land-hdpi/screen.png',  width : 800, height: 480 },
             { name : 'drawable-land-xhdpi/screen.png', width : 1280, height: 720 },
+            { name : 'drawable-land-xxhdpi/screen.png', width : 1600, height: 960 },
+            { name : 'drawable-land-xxxhdpi/screen.png', width : 1920, height: 1280 },
             { name : 'drawable-port-ldpi/screen.png',  width : 200, height: 320 },
             { name : 'drawable-port-mdpi/screen.png',  width : 320, height: 480 },
             { name : 'drawable-port-hdpi/screen.png',  width : 480, height: 800 },
             { name : 'drawable-port-xhdpi/screen.png', width : 720, height: 1280 },
+            { name : 'drawable-port-xxhdpi/screen.png', width : 960, height: 1600 },
+            { name : 'drawable-port-xxxhdpi/screen.png', width : 1280, height: 1920 },
         ],
         iconPath : 'platforms/android/res/',
         icon : [
-            { name : 'drawable-ldpi/icon.png',   width : 36, height: 36 },
-            { name : 'drawable-mdpi/icon.png',   width : 48, height: 48 },
-            { name : 'drawable-hdpi/icon.png',   width : 72, height: 72 },
-            { name : 'drawable-xhdpi/icon.png',  width : 96, height: 96 },
-            { name : 'drawable-xxhdpi/icon.png', width : 144, height: 144 },
+            { name : 'mipmap-ldpi/icon.png',   width : 36, height: 36 },
+            { name : 'mipmap-mdpi/icon.png',   width : 48, height: 48 },
+            { name : 'mipmap-hdpi/icon.png',   width : 72, height: 72 },
+            { name : 'mipmap-xhdpi/icon.png',  width : 96, height: 96 },
+            { name : 'mipmap-xxhdpi/icon.png', width : 144, height: 144 },
+            { name : 'mipmap-xxxhdpi/icon.png', width : 192, height: 192 },
         ]
     });
     // TODO: add all platforms
